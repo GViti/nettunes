42673231d5c089f52c6c0e4ccfa54d10701d6081
GViti <vitigabriele1@gmail.com>
2024-11-01 01:03:15 +0100

    fifth commit

--- src/app/assets/images/socials.ts
+++ src/app/assets/images/socials.ts
@@ -1,9 +1,9 @@
-const socials = {
-    ig: require("./instagram.svg"),
-    fb: require("./facebook.svg"),
-    yt: require("./youtube.svg"),
-    xt: require("./twitter.svg"),
-  };
+// const socials = {
+//     ig: require("./instagram.svg"),
+//     fb: require("./facebook.svg"),
+//     yt: require("./youtube.svg"),
+//     xt: require("./twitter.svg"),
+//   };
   
-  export default socials;
+//   export default socials;
   
